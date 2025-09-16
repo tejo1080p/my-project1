9470252fb3c5447124e801deed1fb90464a062a8
Tejo <tejogudala1@gmail.com>
2025-09-16 11:44:18 +0530

    commit

---
+++ react_project/Experiment-3.3/src/classes/Person.js
@@ -0,0 +1,12 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  displayInfo() {
+    return `Name: ${this.name}, Age: ${this.age}`;
+  }
+}
+
+export default Person;
